7b1ba472f73b4edac21af41a9907361fa4c1014c
arata83 <arata@uniba.jp>
2024-07-02 19:04:24 +0900

    Update client.rb

--- lib/freee/tags/client.rb
+++ lib/freee/tags/client.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 
 module Freee
   module Api
     class Tags
-      BASE_URL = 'https://api.freee.co.jp'
       PATH = '/api/1/segments/1/tags'
+      PATH.freeze
 
       def initialize
         @client = Faraday.new(url: BASE_URL) do |faraday|
@@ -16,9 +17,19 @@ module Freee
       # タグ一覧の取得
       def get_tags(access_token, company_id)
         raise 'アクセストークンが設定されていません' if access_token.empty?
-        
-        response = @client.get(PATH, { company_id: company_id }, 'Authorization' => "Bearer #{access_token}")
-        handle_response(response, 'fetch')
+        raise '事業所IDが設定されていません' unless params.key?(:company_id)
+        @client.authorization :Bearer, access_token
+        response = @client.get do |req|
+          req.url PATH
+          req.params = params
+        end
+        case response.status
+        when 400
+          raise StandardError, response.body
+        when 401
+          raise 'Unauthorized'
+        end
+        response
       end
 
       # タグの作成
@@ -29,23 +40,7 @@ module Freee
         handle_response(response, 'create')
       end
 
-      private
 
-      def handle_response(response, action)
-        case response.status
-        when 200, 201
-          if action == 'fetch'
-            Rails.logger.info "Tags fetched successfully: #{response.body}"
-            response.body
-          elsif action == 'create'
-            Rails.logger.info "Successfully created tag: segment_tag_id=#{response.body.dig('segment_tag', 'id')}"
-            response.body.dig('segment_tag', 'id')
-          end
-        else
-          Rails.logger.error "Failed to #{action} tags: #{response.body}"
-          nil
-        end
-      end
     end
   end
 end
